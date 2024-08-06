249bb77f048dbe15760fa50d7e52e8b650d6ec4d
淋漓 <2564576814@qq.com>
2024-08-06 21:31:17 +0800

    播放音乐

---
+++ app/src/main/java/com/example/playaudiotest/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.playaudiotest;
+
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.FaceDetector;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initMediaPlayer();
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mediaPlayer.isPlaying()){
+                    mediaPlayer.start();//开始播放
+                }else {
+                    mediaPlayer.pause();//如果是播放状态就暂停
+                }
+            }
+        });
+        Button button1 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.reset();//停止播放
+                initMediaPlayer();//重新回到准备状态
+            }
+        });
+    }
+    private void initMediaPlayer(){
+        AssetManager assetManager = getAssets();
+        try {
+            AssetFileDescriptor faceDetector = assetManager.openFd("music.mp3");
+            mediaPlayer.setDataSource(faceDetector.getFileDescriptor(),faceDetector.getStartOffset(),faceDetector.getLength());
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+}
